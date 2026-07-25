4b58764d498018a6f935adcf5285b19206c8b1bf
Adams Shittu <shttdms@gmail.com>
2026-07-25 19:20:38 +0100

    Anecdotes Exercise Completed

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0) 
+  const [votes, setVotes] = useState(
+    Array(anecdotes.length).fill(0)
+  )
+ 
+  const handleClick = () => {
+    const newSelect = selected + 1
+    setSelected(newSelect)
+    if (newSelect === anecdotes.length) {
+      setSelected(0)
+    }
+  }
+
+  const handleVotes = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    console.log(copy[selected])
+  }
+
+  const largest = Math.max(...votes)
+  const largestIndex = votes.indexOf(largest)
+
+  return (
+    <div>
+      <h2>Anecdote for the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has <button>{votes[selected]}</button> votes</p>
+      <button  onClick={handleVotes}>vote</button>
+      {' '}
+      <button onClick={handleClick}>next anecdote</button>
+
+      {largest > 0 && (
+        <>
+          <h2>Anecdote with the most votes</h2>
+          <p>{anecdotes[largestIndex]}</p>
+          <p>has <button>{votes[largestIndex]}</button> votes</p>
+        </>
+      )}
+      
+
+    </div>
+  )
+}
+
+export default App
